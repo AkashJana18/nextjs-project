7c07fadb9cc7fb5af37af991a80645f8383350c3
AkashJana18 <akashjana663@gmail.com>
2024-07-25 19:32:10 +0530

    nextAuth setup

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,58 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User.model";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.indentifier },
+            ],
+          });
+          if (!user) throw new Error("User not found with this email");
+          if (!user.isVerified)
+            throw new Error("Please verify your account before login");
+          const isPasswordValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isPasswordValid) {
+            throw new Error("Invalid password");
+          } else {
+            return user;
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+        return session
+      },
+      async jwt({ token, user }) {
+        return token
+      }
+  },
+  pages: {
+    signIn: "/sign-in"
+  },
+  session: {
+    strategy: "jwt"
+  },
+  secret: process.env.NEXTAUTH_SECRET
+};
